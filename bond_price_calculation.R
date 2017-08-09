1429d8fb696a5cf59ac673b1b420770743fe7909
Mark Badros <mbadros@gmail.com>
2017-08-09 19:22:35 -0400

    Calculate through latest 6-month period of known composite rate

--- bond_price_calculation.R
+++ bond_price_calculation.R
@@ -30,6 +30,9 @@ ISSUE_DATE <- '2001-10-13'
 YEARS_TO_CALC <-  30
 DENOMINATION <- 1000
 INFLATION_ASSUMED <- 0.013
+VALUE_DATE = as.Date('2017-10-01')
+
+
 
 adj_issue_date <- as.Date(as.yearmon(ISSUE_DATE), frac = 0)
 maturity_date <-  adj_issue_date + years(30)
@@ -43,4 +46,35 @@ composite_rate <- fixed_component + (2 * inflation_period) + (fixed_component * 
 coredata(composite_rate) <-  pmax(0, round(coredata(composite_rate), digits = 4))
 
 PROJ_COMPOSITE <-  round(fixed_component + (2 * INFLATION_ASSUMED) + (fixed_component * INFLATION_ASSUMED), digits = 4)
+
+redemption <- data.frame(Date = seq(from = adj_issue_date, to = min(VALUE_DATE, maturity_date), by = "1 month"),
+                         Bond_value = DENOMINATION,
+                         Interest_base = DENOMINATION,
+                         Monthly_interest = 0,
+                         Unpaid_interest = 0)
+
+g <- seq(adj_issue_date, min(VALUE_DATE, maturity_date), 'month')
+monthly_rates <- zoo(rep(composite_rate / 12, each = 6), order.by = g) ## Check Rounding
+
+
+for(i in 1:(length(g)-1)) {
+    redemption$Monthly_interest[i] <- monthly_rates[i] * redemption$Interest_base[i] %>% round(digits = 4)
+    redemption$Unpaid_interest[i] <- redemption$Unpaid_interest[max(1, i-1)] + redemption$Monthly_interest[i]
+    redemption$Bond_value[i+1] <- redemption$Interest_base[i] + redemption$Unpaid_interest[i]
+
+    redemption$Interest_base[i + 1] <- redemption$Interest_base[i]
+    
+    if(i %% 6 == 0) {
+      redemption$Interest_base[i+1] <- redemption$Interest_base[i] + redemption$Unpaid_interest[i]
+      redemption$Unpaid_interest[i] <- 0
+    }
+}
+
+final_value <- redemption$Bond_value[length(g)] + redemption$Unpaid_interest[length(g) - 1]
+cat("The bond's value at", format(redemption$Date[length(g)], '%B %Y'), 'is')
+paste0('$', prettyNum(final_value, format = 'f', big.mark = ',')
+)
+
+
+
 
